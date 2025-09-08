d036e31675de020ec88a30222d8c4e01a2b0c624
Madhavan247 <madhavanmadhavan541@gmail.com>
2025-09-08 20:36:29 +0530

    fifthproject

---
+++ demo1.java
@@ -0,0 +1,12 @@
+public class demo1 {
+    public static void main(String[] args){
+        int i,j,row=6;
+        for(i=1;i<=row;i++){
+            for(j=i;j<=row;j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+
+    }
+}
